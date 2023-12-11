25951568edbcfdfb8e9fd021eadc6d089f512521
gelzinn <60621494+gelzinn@users.noreply.github.com>
2023-12-11 13:08:02 -0300

    chore(libs): update ` @vercel/analytics` to ` @vercel/speed-insights`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,57 +1,74 @@
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
 
-import { Analytics } from '@vercel/analytics/react';
-import { Inter } from 'next/font/google'
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Inter } from "next/font/google";
 
-import './globals.css'
-import { DocumentSizeContextProvider } from '@/contexts/DocumentSizeContext';
+import "./globals.css";
+import { DocumentSizeContextProvider } from "@/contexts/DocumentSizeContext";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'gelzin · Software Engineer and UX/UI Designer',
-  description: 'Software Engineer and UX/UI Designer',
+  title: "gelzin · Software Engineer and UX/UI Designer",
+  description: "Software Engineer and UX/UI Designer",
   authors: [
     {
-      name: 'gelzin',
-      url: 'https://gelzin.com',
+      name: "gelzin",
+      url: "https://gelzin.com",
     },
   ],
-  applicationName: 'gelzin',
-  keywords: [
-    'software', 'engineer', 'ux', 'ui', 'designer'
-  ],
-  themeColor: '#080808',
-}
+  applicationName: "gelzin",
+  keywords: ["software", "engineer", "ux", "ui", "designer"],
+  themeColor: "#080808",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <head>
         <link rel="shortcut icon" href="/gelzin.svg" type="image/svg+xml" />
 
-        <meta property="twitter:image" content="https://gelzin.com/metadata/og_image.png" />
-        <meta property="twitter:card" content="https://gelzin.com/metadata/og_image.png" />
-        <meta property="twitter:title" content="gelzin · Software Engineer and UX/UI Designer" />
-        <meta property="twitter:description" content="Software Engineer and UX/UI Designer" />
+        <meta
+          property="twitter:image"
+          content="https://gelzin.com/metadata/og_image.png"
+        />
+        <meta
+          property="twitter:card"
+          content="https://gelzin.com/metadata/og_image.png"
+        />
+        <meta
+          property="twitter:title"
+          content="gelzin · Software Engineer and UX/UI Designer"
+        />
+        <meta
+          property="twitter:description"
+          content="Software Engineer and UX/UI Designer"
+        />
 
-        <meta property="og:image" content="https://gelzin.com/metadata/og_image.png" />
-        <meta property="og:title" content="gelzin · Software Engineer and UX/UI Designer" />
-        <meta property="og:description" content="Software Engineer and UX/UI Designer" />
+        <meta
+          property="og:image"
+          content="https://gelzin.com/metadata/og_image.png"
+        />
+        <meta
+          property="og:title"
+          content="gelzin · Software Engineer and UX/UI Designer"
+        />
+        <meta
+          property="og:description"
+          content="Software Engineer and UX/UI Designer"
+        />
         <meta property="og:url" content="https://gelzin.coom" />
       </head>
-      <body
-        className={`${inter.className} overflow-x-hidden`}
-      >
+      <body className={`${inter.className} overflow-x-hidden`}>
         <DocumentSizeContextProvider>
           {children}
-          <Analytics />
+          <SpeedInsights />
         </DocumentSizeContextProvider>
       </body>
     </html>
-  )
+  );
 }
