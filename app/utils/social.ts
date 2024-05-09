8130d94e75215bd3d3dc9f0b673b497a905cf016
gelzinn <mcnoobend@gmail.com>
2024-05-09 17:23:56 -0300

    fix: rename `@types` to `types` to resolve problem with new parallel routes

--- app/utils/social.ts
+++ app/utils/social.ts
@@ -1,16 +1,16 @@
-import { ISocial } from "@/@types/social";
+import { ISocial } from '@/types/social';
 
 export const socials: ISocial[] = [
   {
     name: 'GitHub',
-    url: 'https://github.com/gelzinn'
+    url: 'https://github.com/gelzinn',
   },
   {
     name: 'LinkedIn',
-    url: 'https://linkedin.com/in/gelzin'
+    url: 'https://linkedin.com/in/gelzin',
   },
   {
     name: 'Twitter',
-    url: 'https://twitter.com/gelzinn_'
-  }
-]
+    url: 'https://twitter.com/gelzinn_',
+  },
+];
